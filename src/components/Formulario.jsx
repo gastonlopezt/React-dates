7ae7179bd9a815a09febb2ddbe821a2272c0bdcf
gastonlopezt <gaston_lopeztisera@hotmail.com>
2022-04-06 14:45:18 -0300

    first commit

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Formulario() {
+  return (
+    <div className="md:w-1/2 lg:w-2/5">
+      <h2 className="font-black text-3xl text-center">Seguimiento pacientes</h2>
+      <p className="text-lg mt-5 text-center">
+        Añade Pacientes y {""}
+        <span className="text-indigo-600 font-bold">Administralos</span>
+      </p>
+      <form>
+        
+      </form>
+    </div>
+  );
+}
+
+export default Formulario
